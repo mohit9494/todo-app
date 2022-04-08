858fd14df4970570f12d8b959e6b872b0ce404f0
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-04-07 22:35:25 -0400

    Added Router V6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
-import Header from './MyComponents/Header.js';
-import Todos from './MyComponents/Todos';
-import React, { useState, useEffect } from 'react';
-import AddTodo from './MyComponents/AddTodo';
+import "./App.css";
+import Header from "./MyComponents/Header.js";
+import Todos from "./MyComponents/Todos";
+import React, { useState, useEffect } from "react";
+import AddTodo from "./MyComponents/AddTodo";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./MyComponents/About";
 
 function App() {
-
   let initTodos;
 
   if (localStorage.getItem("todos") === null) {
@@ -15,7 +16,6 @@ function App() {
   }
 
   const addTodo = (title, desc) => {
-
     let id;
 
     if (todos.length === 0) {
@@ -27,35 +27,47 @@ function App() {
     const myTodo = {
       id: id,
       title: title,
-      description: desc
-    }
+      description: desc,
+    };
 
     setTodos([...todos, myTodo]);
-  }
+  };
 
   const onDelete = (todo) => {
-
     console.log("I am deleting ", todo);
 
-    setTodos(todos.filter((e) => {
-
-      return e !== todo;
-
-    }))
-
-  }
+    setTodos(
+      todos.filter((e) => {
+        return e !== todo;
+      })
+    );
+  };
 
   let [todos, setTodos] = useState(initTodos);
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos])
+  }, [todos]);
 
   return (
     <>
-      <Header title="Todos" searchBar={false} />
-      <AddTodo addTodo={addTodo} />
-      <Todos todos={todos} onDelete={onDelete} />
+      <Router>
+        <Header title="Todos" searchBar={false} />
+
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <AddTodo addTodo={addTodo} />
+                <Todos todos={todos} onDelete={onDelete} />
+              </>
+            }
+          />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div>
+            This is about Component
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque officiis est asperiores quia enim. Quam, qui labore sint ex enim similique possimus adipisci quod hic tempore provident animi, et debitis?</p>
+
+        </div>
+    )
+}
